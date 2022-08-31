0bd4d975487b5e862cdb42d504afea584746b7f1
GHOST <ghostdevbusiness@gmail.com>
2022-08-31 19:41:37 +0000

    feat: install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+# Install volta
+curl https://get.volta.sh | bash
+
+# Install global tools
+volta install pnpm
+volta install tsc
+volta install create-ghost
+volta install astro
+volta install nodemon
