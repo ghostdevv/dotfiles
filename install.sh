2290c78e25a646ccde3d0bf3fffa687cb5a0fc2b
GHOST <ghostdevbusiness@gmail.com>
2022-10-31 00:57:35 +0000

    fix: remove create-ghost

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ volta setup
 volta install node
 
 # Install global tools
-volta install pnpm tsm create-ghost nodemon yarn
+volta install pnpm yarn tsm nodemon
 
 # Load aliases
 cp "$CURRENT_DIR/.bash_aliases" ~/.bash_aliases
