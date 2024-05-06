1e6d1ebc79604338db9536ba48042c8e3cc4352c
GHOST <ghostdevbusiness@gmail.com>
2024-05-06 19:54:18 +0100

    feat: overhaull installation script

--- install.sh
+++ install.sh
@@ -5,27 +5,26 @@ CURRENT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 
 # Logs
 echo "Installing dotfiles"
-echo "B_S: ${BASH_SOURCE} S: $SHELL CD: $CURRENT_DIR"
+printf "in: '$SHELL' at: '$CURRENT_DIR'\n\n"
 
-# Install volta
-curl https://get.volta.sh | bash
+# Install & Setup volta
+if ! command -v bat &> /dev/null; then
+    curl https://get.volta.sh | bash
+    VOLTA_HOME="$HOME/.volta"
+    PATH="$VOLTA_HOME/bin:$PATH"
 
-# Load volta executable
-VOLTA_HOME="$HOME/.volta"
-PATH="$VOLTA_HOME/bin:$PATH"
+    volta setup
+    volta install node@20
+    volta install pnpm tsm nodemon @antfu/ni
+fi
 
-# Setup volta
-volta setup
-volta install node@20
+# Download & Update dotfiles
+source "$CURRENT_DIR/src/.dotfiles.sh"
+update_dotfiles
 
-# Install global tools
-volta install pnpm tsm nodemon @antfu/ni
+if [ "$(basename $SHELL)" = "bash" ]; then
+    source ~/.bashrc
+fi
 
-# Load aliases
-cp "$CURRENT_DIR/.bash_aliases" ~/.bash_aliases
-
-# Reload bash
-. ~/.bashrc
-
-# Done
-echo "Done!"
+printf "\n\n\nPlease include the following at the top of your shell config file:\n\n"
+cat "$CURRENT_DIR/src/.zshrc"
