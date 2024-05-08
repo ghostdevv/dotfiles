c2cf08f955d8b36d20733174f116c8e2aebea386
GHOST <ghostdevbusiness@gmail.com>
2024-05-08 17:08:49 +0100

    refactor: store version in seperate file

--- src/.dotfiles.sh
+++ src/.dotfiles.sh
@@ -14,34 +14,26 @@ update_dotfiles() {
     esac
   done
 
+  local STORAGE_DIR="$HOME/.dotfiles"
+
   local LATEST_COMMIT="$(git ls-remote --head https://github.com/ghostdevv/dotfiles.git --ref main --type commit | head -n 1 | awk '{print $1}')"
   printf "\nUpdating Dotfiles ($(echo $LATEST_COMMIT | cut -c1-7)) [$FORCE]\n\n"
 
-  function comment {
-    case $1 in
-      *.css)   echo "/* $2 */" ;;
-      *.json)  exit 1          ;;
-      *)       echo "# $2"     ;;
-    esac
-  }
+  if [[ ! -d "$STORAGE_DIR" ]]; then
+    mkdir -p "$STORAGE_DIR"
+    touch "$STORAGE_DIR/version"
+  fi
+
+  local CURRENT_VERSION="$(cat "$STORAGE_DIR/version")"
 
   function dotfiles_download {
     local OUTPUT="$HOME/$1"
 
-    if [[ -f "$OUTPUT" ]]; then
-      local CURRENT_COMMIT="$(grep -m 1 -Eo 'DOTFILES_VERSION=(\w+)' $OUTPUT | sed 's/DOTFILES_VERSION=//')"
-    fi
-
-    if [[ "$LATEST_COMMIT" != "$CURRENT_COMMIT" || "$FORCE" = true ]]; then
+    if [[ "$LATEST_COMMIT" != "$CURRENT_VERSION" || "$FORCE" = true || ! -f "$OUTPUT" ]]; then
       printf "\n\nUpdating '$1'\n"
       mkdir -p "$(dirname $OUTPUT)"
-      curl -L "https://raw.githubusercontent.com/ghostdevv/dotfiles/$LATEST_COMMIT/src/$1" -o $OUTPUT
-      
-      local COMMENT="$(comment $OUTPUT "DOTFILES_VERSION=$LATEST_COMMIT")"
-      if [[ "$(uname)" != "Darwin" && -n "$COMMENT" ]]; then
-        # this isn't working correctly on mac and I can't fix it
-        sed -i "1i$COMMENT\n" $OUTPUT
-      fi
+      curl -L "https://raw.githubusercontent.com/ghostdevv/dotfiles/$LATEST_COMMIT/src/$1" -o "$OUTPUT"
+      echo
     else
       echo "Skipping '$1'"
     fi
@@ -57,10 +49,12 @@ update_dotfiles() {
   dotfiles_download ".config/fastfetch/config.jsonc"
 
   # Linux Specific
-  if [[ "$(uname)" != "Darwin" ]]; then
+  if [[ "$(uname)" = "Linux" ]]; then
     dotfiles_download ".themes/GHOST/gnome-shell/gnome-shell.css"
     dotfiles_download ".config/presets/user/ghost.json"
   fi
+
+  echo "$LATEST_COMMIT" > "$STORAGE_DIR/version"
 
   printf "\nDone! Don't forget to restart your shell.\n"
 }
