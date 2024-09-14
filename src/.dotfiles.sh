78c973df3d418f1f5e98a23a8b14185326225327
GHOST <ghostdevbusiness@gmail.com>
2024-09-14 04:27:04 +0100

    fix: add guake to dotfiles download

--- src/.dotfiles.sh
+++ src/.dotfiles.sh
@@ -47,6 +47,7 @@ update_dotfiles() {
   dotfiles_download ".nanorc"
   dotfiles_download ".config/alacritty/alacritty.toml"
   dotfiles_download ".config/fastfetch/config.jsonc"
+  dotfiles_download ".config/guake/guake.conf"
 
   # Linux Specific
   if [[ "$(uname)" = "Linux" ]]; then
