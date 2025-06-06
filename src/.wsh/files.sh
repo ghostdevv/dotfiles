d9158f17845e363dea946303216545b14a7f7dd5
GHOST <ghostdevbusiness@gmail.com>
2025-06-07 00:35:04 +0100

    fix: fw expansion card image size

--- src/.wsh/files.sh
+++ src/.wsh/files.sh
@@ -54,4 +54,4 @@ function quish_simple() {
   ffmpeg -y -v quiet -stats -i "$file" -vf "scale=$resolution:force_original_aspect_ratio=decrease,pad=$resolution:(ow-iw)/2:(oh-ih)/2" -r "$fps" -b:v "$bitrate"k "${output}.${type}" </dev/null
 }
 
-alias fw="viu ~/.wsh/images/framework-16-expansion-cards.png"
+alias fw="viu ~/.wsh/images/framework-16-expansion-cards.png --width 45"
