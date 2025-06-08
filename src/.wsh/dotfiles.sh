7ff6de9af9a948512b46ca5aad6232c1209c38f9
GHOST <ghostdevbusiness@gmail.com>
2025-06-08 20:46:11 +0100

    feat: add pop-shell config

--- src/.wsh/dotfiles.sh
+++ src/.wsh/dotfiles.sh
@@ -89,6 +89,7 @@ function update-dotfiles() {
   # Tools
   _dotfiles_download ".config/fastfetch/config.jsonc"
   _dotfiles_download ".config/fw-fanctrl/config.json" "linux"
+  _dotfiles_download ".config/pop-shell/config.json" "linux"
   # Theme
   _dotfiles_download ".themes/GHOST/gnome-shell/gnome-shell.css" "linux"
   # Autostart
