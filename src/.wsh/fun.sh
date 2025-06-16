17ce8046957a4a4bb62b2d22c1f674cb5d1d70a5
GHOST <ghostdevbusiness@gmail.com>
2025-06-16 03:58:43 +0100

    feat: ai-image command

--- src/.wsh/fun.sh
+++ src/.wsh/fun.sh
@@ -9,3 +9,42 @@ function imagine() {
 
     viu --width 45 --frame-rate 12 "$output"
 }
+
+function ai-image() {
+    local PROMPT="$@";
+
+    if [[ -z "$PROMPT" ]]; then
+        echo "Error: No prompt provided."
+        return 1
+    fi
+
+    local ID="$(openssl rand -hex 16)"
+    local OUTPUT="/tmp/ai-images/$ID"
+    local SEED="$((1 + $RANDOM % 100))"
+
+    echo -e "ai-image generator"
+    echo -e "  prompt: \"$PROMPT\""
+    echo -e "  seed:   $SEED"
+    echo -e "  output: $OUTPUT.png"
+    echo -e ""
+
+    mkdir -p "$(dirname $OUTPUT)"
+
+    printf "Generating image..."
+
+    local CLOUDFLARE_ACCOUNT_ID="$(op item get 'ai-image command' --format json --fields 'username' | jq .value --raw-output)"
+    local CLOUDFLARE_API_TOKEN="$(op item get 'ai-image command' --format json --fields 'credential' | jq .value --raw-output)"
+
+    curl https://api.cloudflare.com/client/v4/accounts/$CLOUDFLARE_ACCOUNT_ID/ai/run/@cf/black-forest-labs/flux-1-schnell  \
+        -X POST  \
+        -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN"  \
+        -d "{ \"seed\": \"$SEED\", \"prompt\": \"$PROMPT\" }" \
+        -o "$OUTPUT.json" \
+        --progress-bar
+
+    echo -e ""
+
+    jq .result.image "$OUTPUT.json" --raw-output | base64 -d > "$OUTPUT.png"
+
+    viu "$OUTPUT.png" --width 45
+}
