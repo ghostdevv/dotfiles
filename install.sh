ba3462cc01cc02ec97e9fe986dc2c948cb181ea1
Willow (GHOST) <git@willow.sh>
2026-04-29 22:45:45 +0100

    chore: switch to fnm

--- install.sh
+++ install.sh
@@ -7,19 +7,6 @@ CURRENT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 echo "Installing dotfiles"
 printf "in: '$SHELL' at: '$CURRENT_DIR'\n\n"
 
-# Install & Setup volta
-if ! command -v volta &> /dev/null; then
-    curl https://get.volta.sh | bash
-
-    export VOLTA_FEATURE_PNPM="1"
-    export VOLTA_HOME="$HOME/.volta"
-    export PATH="$VOLTA_HOME/bin:$PATH"
-
-    volta setup
-    volta install node@22
-    volta install pnpm@10 tsx typescript nodemon @antfu/ni
-fi
-
 # Install & Setup Deno
 if ! command -v deno &> /dev/null; then
     curl -fsSL https://deno.land/install.sh | sh
