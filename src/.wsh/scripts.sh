a0fe1bc1d0a4e14b18668b4d59d532b2f5c0bede
Willow (GHOST) <git@willow.sh>
2026-08-19 03:13:16 +0100

    fix: update-crates command asking for more perms

--- src/.wsh/scripts.sh
+++ src/.wsh/scripts.sh
@@ -1,6 +1,7 @@
 function update-crates() {
     deno run \
         --allow-read=Cargo.toml --allow-write=Cargo.toml \
-        --allow-env=TERM,CI --allow-net=crates.io \
+        --allow-env=NODE_DISABLE_COLORS,TMUX,TF_BUILD,TEAMCITY_VERSION,TERM_PROGRAM,COLORTERM,TERM,CI \
+        --allow-net=crates.io \
         "$HOME/.wsh/scripts/update-crates.ts"
 }
