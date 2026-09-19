f53a09a0f3d1cc76f825a1e73b0ae86bd94d083e
Willow (GHOST) <git@willow.sh>
2026-09-19 13:50:18 +0100

    feat: pkgrep supports flatpak

--- src/.wsh/system.sh
+++ src/.wsh/system.sh
@@ -26,7 +26,6 @@ alias ll="ls -alFh"
 alias l="ls"
 
 alias update-mirrors="sudo reflector @/etc/xdg/reflector/reflector.conf"
-alias pkgrep="yay -Q | grep -i"
 
 # bat config
 if command -v bat &> /dev/null; then
@@ -42,3 +41,32 @@ alias sl="sl -d -e"
 
 # Show the framework 16 expansion card support image
 alias fw="viu ~/.wsh/images/framework-13-expansion-cards.png --width 45"
+
+function __wsh_pkg_str() {
+	if [[ $1 -eq 1 ]]; then
+		echo "package"
+	else
+		echo "packages"
+	fi
+}
+
+function pkgrep() {
+	local flatpak="$(flatpak list --app --columns "name,application,versio" | grep --color=always -i "$@")"
+	local flatpak_count="$(echo "$flatpak" | wc -l)"
+
+	local arch="$(yay -Q | grep --color=always -i "$@")"
+	local arch_count="$(echo "$arch" | wc -l)"
+
+	if [[ -n "$arch" && -n "$flatpak" ]]; then
+		echo -e "Found $arch_count Arch $(__wsh_pkg_str $arch_count) and $flatpak_count Flatpak $(__wsh_pkg_str $flatpak_count):"
+		echo -e "$arch\n$flatpak"
+	elif [[ -n "$arch" ]]; then
+		echo -e "Found $arch_count Arch $(__wsh_pkg_str $arch_count):"
+		echo -e "$arch"
+	elif [[ -n "$flatpak" ]]; then
+		echo -e "Found $flatpak_count Flatpak $(__wsh_pkg_str $flatpak_count):"
+		echo -e "$flatpak"
+	else
+		echo -e "No Arch or Flatpak packages found"
+	fi
+}
